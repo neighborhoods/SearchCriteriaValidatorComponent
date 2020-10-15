f89ac91e28934d76e583b97aec352144ce386d82
Ariel Allon <arielallon@users.noreply.github.com>
2020-10-15 00:51:46 -0500

    Add addFactory to ValidatorVisitor builder/interface
    - Allows adding decorator factories

--- src/ValidatorVisitor/BuilderInterface.php
+++ src/ValidatorVisitor/BuilderInterface.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 namespace Neighborhoods\SearchCriteriaValidatorComponent\ValidatorVisitor;
 
 use Neighborhoods\SearchCriteriaValidatorComponent\ValidatorVisitorInterface;
+use Neighborhoods\SearchCriteriaValidatorComponent\ValidatorVisitor\Decorator\FactoryInterface as DecoratorFactoryInterface;
 
 interface BuilderInterface
 {
     public function build(): ValidatorVisitorInterface;
 
-    public function setRecord(array $record): BuilderInterface;
+    public function addFactory(DecoratorFactoryInterface $decoratorFactory): BuilderInterface; /** @override */
 }
